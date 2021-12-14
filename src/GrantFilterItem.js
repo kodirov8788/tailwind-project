16cddc102c439b15190d66d0c88549400f8e5109
Kodirov8788@gmail.com <kodirov8788@gmail.com>
2021-12-15 00:02:47 +0500

    added select

--- src/GrantFilterItem.js
+++ src/GrantFilterItem.js
@@ -1,13 +1,18 @@
-import React from "react";
-import { BiChevronDown } from "react-icons/bi";
-const GrantFilterItem = ({ text, subtext }) => {
+import React, { useState } from "react";
+const GrantFilterItem = ({ text, subtext, data }) => {
+  const [prData, setPrData] = useState();
   return (
-    <div className="w-[200px] h-[60px] mr-[7px] bg-[#F8F8F8] flex p-3 rounded-[10px] justify-between">
-      <div>
-        <p className="text-[10px] text-[#8D8D8D]">{subtext}</p>
-        <h1 className="text-bold-500 text-[14px]">{text}</h1>
-      </div>
-      <BiChevronDown className="text-[20px]  block mt-auto mb-auto" />
+    <div className="relative bg-[#F8F8F8] w-[200px] h-[60px] mr-[7px]   p-2 rounded-[10px]">
+      <p className="text-[10px] ml-1 text-[#8D8D8D]">{subtext}</p>
+      <select
+        onChange={(e) => setPrData(e.target.value)}
+        className="absolute bg-transparent w-[90%] top-[10%] h-full border-none outline-none"
+        name={prData}
+      >
+        {data.map((item) => (
+          <option key={item.id}>{item.pname}</option>
+        ))}
+      </select>
     </div>
   );
 };
--- src/MainGrantFilters.js
+++ src/MainGrantFilters.js
@@ -3,12 +3,42 @@ import { FiDownload } from "react-icons/fi";
 import GrantFilterItem from "./GrantFilterItem";
 
 const MainGrantFilters = () => {
+  const data = [
+    {
+      id: 1,
+      pname: "shoes",
+    },
+    {
+      id: 2,
+      pname: "shoes 2",
+    },
+    {
+      id: 3,
+      pname: "shoes 3",
+    },
+    {
+      id: 4,
+      pname: "shoes 4",
+    },
+  ];
   return (
     <div className="w-[900px] flex justify-between">
-      <GrantFilterItem text={"open"} subtext={"Grant Status"} />
-      <GrantFilterItem text={"Review Status"} subtext={"Revise and Resubmit"} />
-      <GrantFilterItem text={"Payment Status:"} subtext={"No Payment Due"} />
-      <GrantFilterItem text={"Ball in Court:"} subtext={"Grantee"} />
+      <GrantFilterItem data={data} text={"open"} subtext={"Grant Status"} />
+      <GrantFilterItem
+        data={data}
+        text={"Review Status"}
+        subtext={"Revise and Resubmit"}
+      />
+      <GrantFilterItem
+        data={data}
+        text={"Payment Status:"}
+        subtext={"No Payment Due"}
+      />
+      <GrantFilterItem
+        data={data}
+        text={"Ball in Court:"}
+        subtext={"Grantee"}
+      />
       <div className="w-[72px] h-[60px]  bg-[#1D1D1D] text-white flex p-3 rounded-[10px] justify-center">
         <FiDownload className="text-[18px] block mt-auto mb-auto" />
       </div>
